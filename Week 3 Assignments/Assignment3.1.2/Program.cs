23e52ba8b490acc7746d1847b9c37583260c2684
Chase Young <chase.young1@outlook.com>
2024-05-20 14:21:20 -0500

    Assignment3.1.2

--- Week 3 Assignments/Assignment3.1.2/Program.cs
+++ Week 3 Assignments/Assignment3.1.2/Program.cs
@@ -10,7 +10,7 @@
         }
         static bool IsLeapYear(int year)
         {
-            return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+            return (DateTime.IsLeapYear(year));
         }
     }
 }

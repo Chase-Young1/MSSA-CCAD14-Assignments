b2834b1fb1c4eafc41367347320763d8e9c67e6a
Chase Young <chase.young1@outlook.com>
2024-05-20 15:11:45 -0500

    Assignment3.1.3

---
+++ Week 3 Assignments/Assignment3.1.3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Assignment3._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a string.");
+            string inputString = Console.ReadLine();
+
+            int spaceCount = CountSpaces(inputString);
+
+            Console.WriteLine($"{inputString} contains {spaceCount} spaces.");
+        }
+        static int CountSpaces(string s)
+        {
+            int count = 0;
+            foreach (char c in s)
+            {
+                if (c == ' ')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}

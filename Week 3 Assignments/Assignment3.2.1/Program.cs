01908c8374ad4a804a6395de2eb4c46b04b5803c
Chase Young <chase.young1@outlook.com>
2024-05-21 11:28:09 -0500

    Assignment3.2.1

---
+++ Week 3 Assignments/Assignment3.2.1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Assignment3._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] my2DArray = { { 2, 3, 4 }, { 1, 4, 6 } };
+
+            for (int i = 0; i < my2DArray.GetLength(0); i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < my2DArray.GetLength(1); j++)
+                {
+                    Console.Write(my2DArray[i, j] + "|");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Week 3 Assignments/Assignment3.2.2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Assignment3._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] firstMatrix = { { 1, 2 }, { 3, 4 } };
+            int[,] secondMatrix = { { 5, 6, }, { 7, 8 } };
+
+            Console.WriteLine("The first matrix:");
+            Console.WriteLine();
+            for (int i = 0; i < firstMatrix.GetLength(0); i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < firstMatrix.GetLength(1); j++)
+                {
+                    Console.Write(firstMatrix[i, j] + "|");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("The second matrix:");
+            Console.WriteLine();
+            for (int i = 0; i < secondMatrix.GetLength(0); i++)
+            {
+                Console.Write("|");
+                for (int j = 0;j < secondMatrix.GetLength(1); j++)
+                {
+                    Console.Write(secondMatrix[i, j] + "|");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("The addition of the two matrices is:");
+            Console.WriteLine();
+            for (int i = 0; i < firstMatrix.GetLength(0); i++ )
+            {
+                Console.Write("|");
+                for (int j = 0; j < firstMatrix.GetLength(1); j++)
+                {
+                    Console.Write(firstMatrix[i, j] + secondMatrix[i, j] + "|");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

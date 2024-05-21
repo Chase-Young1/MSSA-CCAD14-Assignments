a4d282c374b33b139cff5578eb6e128c035743d4
Chase Young <chase.young1@outlook.com>
2024-05-21 16:29:54 -0500

    Assignment3.1.4

---
+++ Week 3 Assignments/Assignment3.2.4/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Assignment3._2._4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] inputArray = { 0, 0, 0, 0 };
+
+            Console.WriteLine("Enter the first number:");
+            inputArray[0] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            inputArray[1] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number:");
+            inputArray[2] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the fourth number:");
+            inputArray[3] = Convert.ToInt32(Console.ReadLine());
+
+            double[] outputArray = TotalAverage(inputArray);
+
+            Console.WriteLine($"The average is: {outputArray[0]}");
+            Console.WriteLine($"The total is: {outputArray[1]}");
+        }
+        static double[] TotalAverage(int[] input)
+        {
+            double total = input[0] + input[1] + input[2] + input[3];
+            double average = total / 4;
+            double[] outputArray = { average, total };
+            return outputArray;
+            
+        }
+    }
+}

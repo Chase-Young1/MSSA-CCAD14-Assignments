ec916558ac50bb580ba87ac21f94e4d6b77df7ec
Chase Young <chase.young1@outlook.com>
2024-05-23 20:04:35 -0500

    Assignment3.2.5

--- Week 3 Assignments/Assignment3.2.5/Program.cs
+++ Week 3 Assignments/Assignment3.2.5/Program.cs
@@ -4,11 +4,20 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(GetIndex(new int[] { 1, 5, 3 }, 5)); //First array
+            Console.WriteLine(GetIndex(new int[] { 9, 8, 3 }, 3)); //Second array
+            Console.WriteLine(GetIndex(new int[] { 1, 2, 3 }, 4)); //Third array
         }
-        static void GetIndex()
+        static int GetIndex(int[] inputArray, int index)
         {
-            int[] ints
+            for (int i = 0; i < inputArray.Length; i++) //Loops through the given array to find the index of a given item
+            {
+                if (inputArray[i] == index) //Says if the index equals the item we are searching for, it returns that index
+                {
+                    return i; //If the item is found, return the index
+                }
+            }
+            return -1; //If the item is not found, return -1
         }
     }
 }

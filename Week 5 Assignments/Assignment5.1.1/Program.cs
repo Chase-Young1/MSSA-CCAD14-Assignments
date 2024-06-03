d8a6df3ece5f1c78ec5dc4a7ba19f27a13752f54
Chase Young <chase.young1@outlook.com>
2024-06-03 13:39:11 -0500

    Assignment5.1.1

---
+++ Week 5 Assignments/Assignment5.1.1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Assignment5._1._1
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a valid integer");
+            string x = Console.ReadLine();
+
+            if (FindPalindrome(x))
+            {
+                Console.WriteLine($"{x} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{x} is not a palindrome.");
+            }
+        }
+
+        static bool FindPalindrome(string x)
+        {
+            bool isPalidrome = false;
+
+            for (int i = 0; i < x.Length / 2; i++)
+            {
+                if (!(x[i] == x[x.Length - 1 - i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+            
+        }
+    }
+}

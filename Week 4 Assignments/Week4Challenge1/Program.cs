bf72499041dbc556cdbc3c277e50c21effb54af7
Chase Young <chase.young1@outlook.com>
2024-06-03 09:22:14 -0500

    Week4Challenge2

---
+++ Week 4 Assignments/Week4Challenge1/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Week4Challenge1
+{
+    internal class Program
+    {
+        static double total;
+        static double unitsConsumed;
+        static double unitCost;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the total units consumed.");
+            unitsConsumed = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Total amount due: {total}");
+            
+            ElectricityBill();
+        }
+        static double ElectricityBill()
+        { 
+            switch (total)
+            {
+                case 1:
+                    if (unitsConsumed >= 1 && unitsConsumed <= 199)
+                    {
+                        unitCost = 1.20;
+                        total = unitsConsumed * unitCost;
+                    }
+                break;
+                case 2:
+                    if (unitsConsumed >= 200 &&  unitsConsumed <= 399)
+                    {
+                        unitCost = 1.50;
+                        total = unitsConsumed * unitCost;
+                    }
+                break;
+                case 3:
+                    if (unitsConsumed >= 400 && unitsConsumed <= 599)
+                    {
+                        unitCost = 1.80;
+                        total = unitsConsumed * unitCost;
+                    }
+                break;
+                case 4:
+                    if (unitsConsumed >= 600)
+                    {
+                        unitCost = 2.00;
+                        total = unitsConsumed * unitCost;
+                    }
+                break;
+            }
+            return total;
+        }
+    }
+}
---
+++ Week 4 Assignments/Week4Challenge2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Week4Challenge2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FrequencyOfElements();
+        }
+        private static void FrequencyOfElements()
+        {
+            int[] elementArray = { 25, 12, 43, 12, 15, 25 };
+
+            Dictionary<int, int> elementDictionary = new Dictionary<int, int>();
+
+            foreach (int element in elementArray)
+            {
+                if (elementDictionary.ContainsKey(element))
+                {
+                    elementDictionary[element]++;
+                }
+                else
+                {
+                    elementDictionary[element] = 1;
+                }
+            }
+            foreach (KeyValuePair<int, int> pair in elementDictionary)
+            {
+                Console.WriteLine(format: "{0} occurs {1} times.", pair.Key, pair.Value);
+            }
+        }
+    }
+}

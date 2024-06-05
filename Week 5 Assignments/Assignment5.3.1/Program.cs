a6b3d9e7902def5cfe4ee626315fedeec6cd2e0a
Chase Young <chase.young1@outlook.com>
2024-06-05 16:00:13 -0500

    Assignment5.3.2

---
+++ Week 5 Assignments/Assignment5.3.1/Program.cs
@@ -0,0 +1,51 @@
+﻿/* You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+Example 1:
+
+Input: flowerbed = [1, 0, 0, 0, 1], n = 1
+
+Output: true
+
+Example 2:
+
+Input: flowerbed = [1, 0, 0, 0, 1], n = 2
+
+Output: false */
+
+namespace Assignment5._3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] flowerbed = { 1, 0, 0, 0, 1 };
+            int n1 = 1;
+            Console.WriteLine(PlantFlowers(flowerbed, n1));
+
+            int[] flowerbed2 = { 1, 0, 0, 0, 1 };
+            int n2 = 2;
+            Console.WriteLine(PlantFlowers(flowerbed2, n2));
+
+            int[] flowerbed3 = { 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0 };
+            int n3 = 3;
+            Console.WriteLine(PlantFlowers(flowerbed3, n3));
+
+        }
+        static bool PlantFlowers(int[] flowerbed, int n)
+        {
+            int count = 0;
+
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+                {
+                    flowerbed[i] = 1;
+                    count++;
+                }
+            }
+            return count >= n;
+        }
+    }
+}
---
+++ Week 5 Assignments/Assignment5.3.2/Program.cs
@@ -0,0 +1,61 @@
+﻿/* You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
+Example 1:
+
+Input: n = 2
+
+Output: 2
+
+Explanation: There are two ways to climb to the top.
+
+1. 1 step + 1 step
+
+2. 2 steps
+
+
+
+Example 2:
+
+Input: n = 3
+
+Output: 3
+
+Explanation: There are three ways to climb to the top.
+
+1. 1 step + 1 step + 1 step
+
+2. 1 step + 2 steps
+
+3. 2 steps + 1 step */
+
+namespace Assignment5._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number of steps in a specified staircase.");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(ClimbSteps(n));
+        }
+        static int ClimbSteps(int n)
+        {
+            if (n < 3) //If n == 0, 1, or 2 then the number of ways to climb is == n
+            {
+                return n;
+            }
+            int first = 1;
+            int second = 2;
+            for (int i = 2; i < n; i++) //Starts at 2 and increments one number at a time until we reach n
+            {
+                int current = first + second; //Stores sum of first and second numbers throughout the loop
+                first = second; //Assigns the second number to the first number when moving forward
+                second = current; //Assigns the current number to second as we increment through the loop
+            }
+            return second;
+        }
+    }
+}
